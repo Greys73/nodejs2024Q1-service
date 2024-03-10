e20a64111ce2a99f0d24bcae2e109f444dc7206c
Greys73 <v.myazin@gmail.com>
2024-03-10 11:41:59 +0400

    feat: implement documentation Sun, Mar 10, 2024 11:41:59 AM

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yaml';
 
 dotenv.config();
 const PORT = parseInt(process.env.PORT, 10) || 4000;
 
+const yamlContent = fs.readFileSync('./doc/api.yaml', 'utf-8');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const yamlDoc = YAML.parse(yamlContent);
+  SwaggerModule.setup('doc', app, yamlDoc);
 
   await app.listen(PORT);
 }
